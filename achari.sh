524a0c16aff50846920605377b67ede43adb5af6
xtiankisutsa <xtiankisutsa@github.com>
2017-02-20 08:55:52 +0300

    First commit

---
+++ achari.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+function logo(){
+	echo " "
+	echo "$(figlet -f doom Achari)"
+	echo "Version: 0.1.0 beta"
+	echo "Developed by: Christian Kisutsa"
+	echo ""
+}
+
+
+
+#+++++++
+#ACHARI
+#+++++++
+#Check if option and codes file has been provided
+if ! [ "$1" ] || [ "$1" == '-h' ]  || [ "$1" == '--help' ] || ! [ "$2" ]; then 
+logo
+echo "Usage:"
+echo "[options] <path/to/keep_note/folder>"
+echo ""
+echo "Options:
+-p, --pdf   - convert notes to PDF
+-i, --png   - convert note to PNG file
+-h, --help  - print this help"
+echo " "
+echo "Example:
+Convert to PDF e.g $0 --pdf </full/path/to/keep_note/folder>
+Convert to PNG e.g $0 --png </full/path/to/keep_note/file>"
+
+exit -1
+fi
+
+#++++++++++++++++++++
+#Convert HTML to PDF
+#+++++++++=++++++++++
+if [ $1 == '-p' ] || [ $1 == '--pdf' ] ; then
+
+
+logo
+	
+echo "==================="
+echo " Converting to PDF "
+echo "==================="
+echo "[-]Enter PDF title:"
+read pdf_title
+echo ""
+echo "[-]Enter PDF file name: (without whitespace and .pdf)"
+read pdf_name
+echo ""
+
+echo "[-]Setting up environment..."
+mkdir -p buffer/
+touch buffer/pages.txt
+touch buffer/images.txt
+echo ""
+
+#Find the necessary html files
+echo "[-]Listing files to convert..."
+find $2 -name "page*" -type f -exec ls {} \; | sort -V
+echo ""
+
+echo "[-]Listing images to convert..."
+find $2 -name "*png" -type f -exec ls {} \; | sort -V
+echo ""
+
+echo "[-]Dumping file locations..."
+find $2 -name "page*" -type f -exec ls {} \; | sort -V > buffer/pages.txt
+find $2 -name "*png" -type f -exec ls {} \; | sort -V > buffer/images.txt
+find $2 -name "*jpg" -type f -exec ls {} \; | sort -V >> buffer/images.txt
+echo ""
+
+files_no=`wc -l buffer/pages.txt | cut -d " " -f 1`
+echo "[-]Processing $files_no files...."
+
+count=1
+
+	while read html_file;
+	
+	do	
+		cp "$html_file" buffer/$count.html		
+		count=`expr $count + 1`
+	
+	done <"buffer/pages.txt"
+
+	while read image_file;
+	
+	do	
+		
+		cp "$image_file" buffer/		
+		count=`expr $count + 1`
+	
+	done <"buffer/images.txt"
+echo " "
+
+
+cd tools/
+
+#Get the necessary HTML files
+page_files=`ls ../buffer/*.html | sort -V`
+
+#Generate the PDFs
+echo "[-]Generating shrinked PDF"
+./wkhtmltopdf --title $pdf_title --footer-center [page] -s Letter $page_files "$pdf_name"_small.pdf
+echo " "
+echo "[-]Generating regular PDF"
+./wkhtmltopdf --title $pdf_title --footer-center [page] -s Letter --disable-smart-shrinking $page_files "$pdf_name"_large.pdf
+echo ""
+cd ../
+
+#Tidy up
+echo "[-]Cleaning up.."
+mv tools/"$pdf_name"_small.pdf tools/"$pdf_name"_large.pdf .
+rm -r buffer/
+
+fi
+
+#++++++++++++++++++++
+#Convert HTML to PNG
+#+++++++++=++++++++++
+if [ $1 == '-i' ] || [ $1 == '--png' ] ; then
+
+#Call the necessary functions
+logo
+echo "==================="
+echo " Converting to PNG "
+echo "==================="
+echo "[-]Enter PNG file name: (without whitespace and .png)"
+read png_name
+echo ""
+
+echo "[-]Generating PNG"
+./wkhtmltoimage $2 "$png_name".png
+
+fi
+
+
